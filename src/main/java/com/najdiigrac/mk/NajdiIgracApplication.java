347c3f115156127e5dd2304fecc10a8e95d04908
kirkovg <gjorgjikirkov@gmail.com>
2017-04-11 23:17:22 +0200

    Initial Commit

---
+++ src/main/java/com/najdiigrac/mk/NajdiIgracApplication.java
@@ -0,0 +1,12 @@
+package com.najdiigrac.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NajdiIgracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NajdiIgracApplication.class, args);
+	}
+}
